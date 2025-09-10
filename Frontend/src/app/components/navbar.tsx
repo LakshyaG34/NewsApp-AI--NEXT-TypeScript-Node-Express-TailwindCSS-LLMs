c5c279c6b5f8c6fe0094d77a4172fbeaa29e29a7
Lakshya Goyal <lakshyagoyal34@gmail.com>
2025-09-11 01:27:57 +0530

    Checking pipeline

--- Frontend/src/app/components/navbar.tsx
+++ Frontend/src/app/components/navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () =>{
         <div className="flex flex-row justify-between bg-black/60 px-4 py-6">
             <span className="text-xl font-bold">Nvb</span>
             <div className="flex flex-row gap-2">
-                <Link href="#" className="border border-white rounded-2xl px-1 py-1">LogIn</Link>
+                <Link href="#" className="border border-white rounded-2xl px-1 py-1">Log-In</Link>
                 <Link href="#" className="border border-white rounded-2xl px-1 py-1">Logout</Link>
             </div>
         </div>
